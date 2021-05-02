387d37639e2ed5276ce93e4bfeccaa19be1bd3ff
reza <rezaaa555555r@gmail.com>
2021-05-02 13:46:43 +0430

    ...3

--- pages/posts/_post.vue
+++ pages/posts/_post.vue
@@ -82,6 +82,14 @@
               شما نمی توانید با هیچ کدام از توابع آماده مقداری را داخل یک رشته
               جستجو کنید که 11 رقمی بوده و حتما با 98 شروع شود.
             </v-card-text>
+            <div class="d-flex justify-center">
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+                max-height="460"
+                max-width="500"
+                class="rounded-sm"
+              />
+            </div>
             <v-card-text>
               اینجاست که عبارات با قاعده به کمک شما میاد. عبارات با قاعده به شما
               کمک میکنه متن مورد نظر خودتون رو با الگوی دلخواه خودتون داخل یک
@@ -118,126 +126,6 @@
               اکثر زبان های برنامه نویسی مورد استفاده قرار میگیرد. بریم برای
               ادامه توضیحات.
             </v-card-text>
-          </v-card>
-
-          <v-card class="mb-8">
-            <v-card-title>موارد مرتبط</v-card-title>
-            <section>
-              <v-card class="py-2 px-3" elevation="0">
-                <v-row no-gutters>
-                  <v-col cols="12" sm="4" class="flex-grow-0">
-                    <v-img
-                      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                    ></v-img>
-                  </v-col>
-
-                  <v-col class="p-0 post-card-body">
-                    <v-card-title class="title py-2">
-                      آموزش جاوااسکریپت
-                    </v-card-title>
-                    <v-card-text class="post-card-body">
-                      <v-row class="m-0 mt-1">
-                        <v-rating
-                          :value="3.5"
-                          color="#ffe700"
-                          background-color="warning lighten-1"
-                          dense
-                          half-increments
-                          readonly
-                          size="18"
-                          :empty-icon="icon.StarOutline"
-                          :full-icon="icon.Star"
-                          :half-icon="icon.StarHalfFull"
-                        ></v-rating>
-                        <div class="grey--text">3.5 (413)</div>
-                      </v-row>
-
-                      <div class="mt-auto body-2">
-                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت
-                        چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون
-                        بلکه روزنامه و مجله
-                      </div>
-                      <v-divider class="my-2"></v-divider>
-                      <v-card-actions dir="ltr" class="p-0">
-                        <v-btn
-                          elevation="1"
-                          router
-                          to="/course/css"
-                          color="c_1"
-                          class="ml-3 mb-1"
-                          text
-                        >
-                          ادامه مطالب
-                        </v-btn>
-                        <div class="ml-auto mr-2">
-                          <span class="body-2">45</span>
-                          <v-icon color="c_1">{{
-                            icon.AccountMultiple
-                          }}</v-icon>
-                        </div>
-                      </v-card-actions>
-                    </v-card-text>
-                  </v-col>
-                </v-row>
-              </v-card>
-              <v-card class="py-2 px-3" elevation="0">
-                <v-row no-gutters>
-                  <v-col cols="12" sm="4" class="flex-grow-0">
-                    <v-img
-                      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                    ></v-img>
-                  </v-col>
-
-                  <v-col class="p-0 post-card-body">
-                    <v-card-title class="title py-2">
-                      آموزش جاوااسکریپت
-                    </v-card-title>
-                    <v-card-text class="post-card-body">
-                      <v-row class="m-0 mt-1">
-                        <v-rating
-                          :value="3.5"
-                          color="#ffe700"
-                          background-color="warning lighten-1"
-                          dense
-                          half-increments
-                          readonly
-                          size="18"
-                          :empty-icon="icon.StarOutline"
-                          :full-icon="icon.Star"
-                          :half-icon="icon.StarHalfFull"
-                        ></v-rating>
-                        <div class="grey--text">3.5 (413)</div>
-                      </v-row>
-
-                      <div class="mt-auto body-2">
-                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت
-                        چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون
-                        بلکه روزنامه و مجله
-                      </div>
-                      <v-divider class="my-2"></v-divider>
-                      <v-card-actions dir="ltr" class="p-0">
-                        <v-btn
-                          elevation="1"
-                          router
-                          to="/course/java"
-                          color="c_1"
-                          class="ml-3 mb-1"
-                          text
-                        >
-                          ادامه مطالب
-                        </v-btn>
-                        <div class="ml-auto mr-2">
-                          <span class="body-2">45</span>
-                          <v-icon color="c_1">{{
-                            icon.AccountMultiple
-                          }}</v-icon>
-                        </div>
-                      </v-card-actions>
-                    </v-card-text>
-                  </v-col>
-                </v-row>
-              </v-card>
-            </section>
           </v-card>
           <v-card class="mb-8">
             <div class="d-flex justify-space-between">
